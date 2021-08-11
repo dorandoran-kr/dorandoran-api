80b3551ad0c4e56a73cab0cbab81221390b66ffc
thestarkor <sk_and_mc@kaist.ac.kr>
2021-08-11 20:19:28 +0900

    [init] 초기세팅 및 db

---
+++ src/models/user.js
@@ -0,0 +1,46 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      role: { // 0: user, 1: admin
+        type: Sequelize.INTEGER(4),
+        defaultValue: 0,
+      },
+      id: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING(300),
+        allowNull: true // KAKAO의 경우 password X
+      },
+      nickname: {
+        type: Sequelize.STRING(200),
+        allowNull: false,
+        unique: true
+      },
+      profileUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      }
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'User',
+      tableName: 'users',
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci'
+    });
+  }
+
+  static associate(db) {
+  }
+}
